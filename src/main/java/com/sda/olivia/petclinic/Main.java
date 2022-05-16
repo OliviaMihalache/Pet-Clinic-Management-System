cc96d8d0f9aec56aa61be05d6974902548f88156
DESKTOP-QBKU9KC\John <olivia.mihalache08@gmail.com>
2022-05-16 19:00:34 +0200

    PCMS-12 - Define Hibernate Database Model

--- src/main/java/com/sda/olivia/petclinic/Main.java
+++ src/main/java/com/sda/olivia/petclinic/Main.java
@@ -1,7 +1,12 @@
 package com.sda.olivia.petclinic;
 
+import com.sda.olivia.petclinic.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Temporary change until we have the Repository implementation
+        SessionManager.getSessionFactory().openSession();
 
     }
 }
--- src/main/java/com/sda/olivia/petclinic/model/package-info.java
+++
@@ -1,2 +0,0 @@
-package com.sda.olivia.petclinic.model;
-//hibernate model
