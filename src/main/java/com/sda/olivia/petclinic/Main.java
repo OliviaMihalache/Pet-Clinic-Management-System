e268f0b608e333bc8c9ace9d50e6f0bc410db4cd
DESKTOP-QBKU9KC\John <olivia.mihalache08@gmail.com>
2022-05-17 17:56:58 +0200

    PCMS-1 Create Veterinarian

--- src/main/java/com/sda/olivia/petclinic/Main.java
+++ src/main/java/com/sda/olivia/petclinic/Main.java
@@ -1,14 +1,46 @@
 package com.sda.olivia.petclinic;
 
 import com.sda.olivia.petclinic.controller.VeterinarianController;
+import com.sda.olivia.petclinic.option.UserOption;
 import com.sda.olivia.petclinic.utils.SessionManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         SessionManager.getSessionFactory().openSession();
         VeterinarianController veterinarianController = new VeterinarianController();
-        veterinarianController.create();
-
+        UserOption option = UserOption.UNKNOWN;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            UserOption.printAllOptions();
+            System.out.println("Choose an option");
+            String inputOption = scanner.nextLine();
+            try {
+                int userOption = Integer.parseInt(inputOption);
+                option = UserOption.findBy(userOption).orElse(UserOption.UNKNOWN);
+            } catch (NumberFormatException e) {
+                option = UserOption.UNKNOWN;
+            }
+            System.out.println(option.getPrettyName());
+            switch (option) {
+                case ADD_NEW_VET:
+                    veterinarianController.create();
+                    break;
+                case UNKNOWN:
+                    break;
+                case EXIT:
+                    System.out.println("GOODBYE");
+                    break;
+                default:
+                    System.out.println("This option is not implemented.");
+                    break;
+            }
+        } while (!option.equals(UserOption.EXIT));
+        SessionManager.shutdown();
     }
+
+
 }
+
