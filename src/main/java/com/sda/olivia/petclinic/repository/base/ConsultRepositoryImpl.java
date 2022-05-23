2344b679aa08c65c71296635be593ae207c77e09
DESKTOP-QBKU9KC\John <olivia.mihalache08@gmail.com>
2022-05-23 18:29:48 +0200

    PCMS-16 -Consult with unvaccinated pets

---
+++ src/main/java/com/sda/olivia/petclinic/repository/base/ConsultRepositoryImpl.java
@@ -0,0 +1,2 @@
+package com.sda.olivia.petclinic.repository.base;public class ConsultRepositoryImpl {
+}
---
+++ src/main/java/com/sda/olivia/petclinic/service/ConsultServiceImpl.java
@@ -0,0 +1,2 @@
+package com.sda.olivia.petclinic.service;public class ConsultServiceImpl {
+}
